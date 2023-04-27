306fb261d38ad9e28e98b0ea7d4c9dd335f83587
Josy-Yuuka <yuukatitania@gmail.com>
2023-04-27 08:30:06 -0300

    modificou

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! para modificar");
+       
+        for(int i = 0; i < 11; i++){
+            System.out.println(i);
+        }
     }
 }
