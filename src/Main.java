09c4037116f7487560fe443da26ad9e4236f022f
Josy-Yuuka <yuukatitania@gmail.com>
2023-04-27 08:31:48 -0300

    teste

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
        
         for(int i = 0; i < 11; i++){
-            System.out.println(i);
+            System.out.println(i);  // teste
         }
     }
 }
